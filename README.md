02852564f837c1aaa996ee8e107818d2cb9d6a19
Ruitang Chen <rtchen@Ruitangs-MacBook-Pro.local>
2019-06-17 23:40:54 +0800

    fix sound bug and scheduling bug

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 &nbsp;&nbsp;&nbsp;play midi file while tapping on 'return/enter' key<br/>
 &nbsp;&nbsp;&nbsp;each key is registered as a beat, play audio according<br/>
 &nbsp;&nbsp;&nbsp;to the tempo information from user input<br/>
+need to reinstall fluidsynth
+please see requirement.txt
+or follow instructions from 
+https://stackoverflow.com/questions/20022160/pyfluidsynth-module-object-has-no-attribute-synth
 
--- realtime_playaudio.py
+++ realtime_playaudio.py
@@ -2,11 +2,11 @@ import pretty_midi
 import time
 import threading, time
 import matplotlib.pyplot as plt
-from mingus.midi import fluidsynth   
-from mingus.containers import Note
+import fluidsynth
 import sys
+from scipy import stats
 
-ACC_FILE = 'heyjude.mid'
+ACC_FILE = 'scale.mid'
 BPM = 60
 BPS = BPM/float(60) #beat per second
 original_begin = time.clock()
@@ -21,7 +21,7 @@ pressed_key = "lol"
 timeQueue = []
 stop_thread = False
 sQueue = []
-
+latency_end = -1
 '''
 function: press_key_thread:
 --------------------------------------------------------
@@ -32,17 +32,26 @@ and save to global queue timeQueue
 def press_key_thread():
     global pressed_key
     global stop_thread
+    global latency_end
     while not stop_thread:
         pressed_key = sys.stdin.readline()
         if pressed_key=='\n':
             timeQueue.append(time.clock()-original_begin)
-            if len(timeQueue) == 3:
+            #print timeQueue
+            if len(timeQueue) == 5:
                 b0 = 1
-                t0 = timeQueue[-2]
-                s0 = float(1)/(timeQueue[-1]-timeQueue[-2])
+                t0 = timeQueue[-1]
+                #s0 = float(1)/(timeQueue[-2]-timeQueue[-3])
+                x = timeQueue[-4:]
+                y = range(1,5)
+                s0, intercept, r_value, p_value, std_err = stats.linregress(x,y)
                 sQueue.append(s0)
-            if len(timeQueue)%2==0 and len(timeQueue)>3:
-               b0,t0,s0=compute_tempo_ratio(b0,t0,s0)
+            if len(timeQueue)%2==1 and len(timeQueue)>5:
+                if latency_end == -1:
+                   l = 0.1
+                else:
+                   l = max(0,latency_end - time.clock())
+                b0,t0,s0=compute_tempo_ratio(b0,t0,s0,l)
             pressed_key = 'lol'
 
 
@@ -52,23 +61,24 @@ function: compute_tempo_ratio:
 ----------------------------------------------------
 scheduleing algo by NIME 2011 paper
 '''
-def compute_tempo_ratio(b0,t0,s0):
-    l  =  0.1
+def compute_tempo_ratio(b0,t0,s0,l):
     t1 = timeQueue[-1]
     b = b0+(t1+l-t0)*s0
     tn  = t1+l
     bn = b
     te = timeQueue[-2]
-    se = float(1)/(timeQueue[-1]-timeQueue[-2])
-    be = b0+1
+    #se = float(1)/(timeQueue[-1]-timeQueue[-2])
+    be = len(timeQueue)-5
+    x = timeQueue[-3:]
+    y = range(len(timeQueue)-6,len(timeQueue)-3)
+    se, intercept, r_value, p_value, std_err = stats.linregress(x,y)
     sn  =  (float(4)/(te*se-tn*se-be+bn+4))*se
-    print 'sn %f'%sn
     sQueue.append(sn)
     return bn,tn,sn
 
 
 class Player:
-    def __init__(self, ACC_FILE, original_begin,BPM):
+    def __init__(self, ACC_FILE, original_begin,BPM,fs):
         self.ACC_FILE = ACC_FILE
         self.original_begin = original_begin
         self.midi_data = pretty_midi.PrettyMIDI(ACC_FILE)
@@ -77,6 +87,7 @@ class Player:
         self.noteTimes = []
         self.midi_start_time = self.notes[0].start
         self.BPS = BPM/float(60)
+        self.fs =  fs
 
     '''
     function: follow:
@@ -86,8 +97,9 @@ class Player:
     def follow(self,start):
         global sQueue
         global timeQueue
+        global latency_end
         begin = time.clock()
-
+        
         for i in range(start,len(self.notes)):
             note = self.notes[i]       
             cur_time = time.clock()-begin
@@ -102,8 +114,8 @@ class Player:
                 begin += wait_delta*(tempo_ratio-1)
                 wait_delta = wait_delta *tempo_ratio
 
-
             target_start_time = time.clock() + wait_delta
+            latency_end = target_start_time
             while time.clock() < target_start_time:
                  pass
 
@@ -127,18 +139,18 @@ class Player:
                 delta_time = delta_time *tempo_ratio
 
 
-            n = Note(note.pitch-12)
-            fluidsynth.play_Note(n)
-
+            
+            self.fs.noteon(0,note.pitch,100)
             target_time = time.clock() + delta_time
+            latency_end = target_time
             while time.clock() < target_time:
                  pass
-            fluidsynth.stop_Note(n)
+            self.fs.noteoff(0,note.pitch)
 
 
 
-        tap_time = [t - self.original_begin for t in timeQueue]
-        tap_beat= [t /float(self.BPS) for t in range(len(tap_time))]
+        tap_time = [t  for t in timeQueue]
+        tap_beat= [(t-4) /float(self.BPS) for t in range(len(tap_time))]
 
 
         plt.scatter(x=self.playTimes,y=self.noteTimes,c = 'b',s = 10,marker = 'o')
@@ -161,22 +173,27 @@ class Player:
 if __name__ == '__main__':
     pk_thread = threading.Thread(target = press_key_thread)
     pk_thread.start()
+    fs = fluidsynth.Synth()
+    sfid = fs.sfload("soundfont.sf2")
+    fs.start("coreaudio")
+    fs.program_select(0,sfid,0,0)
     try:
-        fluidsynth.init("soundfont.sf2")
-        print 'tap three times to start'
+        print 'tap four times to start'
         while True:
-            if len(timeQueue)>=3:
+            if len(timeQueue)>=5:
                 break
-        player = Player(ACC_FILE, original_begin,BPM)
+        player = Player(ACC_FILE, original_begin,BPM,fs)
         player.follow(0)
     except KeyboardInterrupt:
         stop_thread = True
         pk_thread.killed = True
         pk_thread.join()
+        fs.delete()
     finally:
         stop_thread = True
         pk_thread.killed = True
         pk_thread.join()
+        fs.delete()
 
 
 
